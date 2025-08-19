5afe0b988d58cb52ec448f04ed2673c42f8b1581
skyen <mkmoen@proton.me>
2025-08-19 19:55:22 +0000

    feat: drag + history

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,10 @@ import Terminal from '@/components/home/terminal'
 
 export default function Home() {
   return (
-    <main className="h-[100dvh] w-full flex flex-col px-4 sm:px-10 justify-center items-center relative">
+    <main className="h-[100dvh] w-full flex flex-col px-4 sm:px-10 justify-center items-center relative overflow-hidden">
       <div className="absolute rounded-full backdrop-blur-lg md:backdrop-blur-3xl w-full h-full -z-10" />
       <div className="absolute opacity-0 animate-fadeIn rounded-full bg-gray-500/10 w-full h-full -z-20 max-w-[clamp(40rem,75vw,80rem)] max-h-[clamp(45rem,90dvh,60rem)]" />
-      <div className="animate-screen-on overflow-hidden max-w-[50rem] max-h-[80dvh] sm:max-h-[37.5rem] rounded-lg border-x-2 border-b-2 border-slate-500 shadow-xl bg-black">
-        <div className="w-full h-10 bg-slate-500" />
-        <Terminal />
-      </div>
+      <Terminal />
     </main>
   )
 }
--- components/home/terminal.tsx
+++ components/home/terminal.tsx
@@ -11,6 +11,8 @@ const startText = 'Hello there...'
 export default function Terminal() {
   const textAreaRef = useRef<HTMLTextAreaElement | null>(null)
   const inputRef = useRef<HTMLInputElement | null>(null)
+  const [toggleState, setToggleState] = useState(0)
+  const [commandHistory, setCommandHistory] = useState<string[]>([])
   const [welcome, setWelcome] = useState<string>('')
   const [history, setHistory] = useState<
     {
@@ -67,9 +69,11 @@ sudo                Gain access to root`,
         },
       ])
 
-      const cmd = commands()[command as keyof ReturnType<typeof commands>]
+      const cmd =
+        commands()[command.toLowerCase() as keyof ReturnType<typeof commands>]
 
       if (cmd) {
+        setCommandHistory((prev) => [...prev, command])
         cmd()
       } else {
         setHistory((prev) => [
@@ -82,6 +86,15 @@ sudo                Gain access to root`,
         inputRef?.current?.scrollIntoView()
       }, 0)
     }
+    if (event.key === 'ArrowUp') {
+      const max = commandHistory.length
+      console.log('max', max)
+      console.log('toggle', toggleState)
+      console.log('history', commandHistory)
+      if (toggleState >= max) return
+      setCommand(commandHistory[toggleState])
+      setToggleState((prev) => toggleState + 1)
+    }
   }
 
   useEffect(() => {
@@ -92,55 +105,113 @@ sudo                Gain access to root`,
     }
   }, [welcome])
 
+  const [offset, setOffset] = useState<{ x: number; y: number }>({ x: 0, y: 0 })
+  const [dragging, setDragging] = useState(false)
+  const windowRef = useRef<HTMLDivElement>(null)
+
+  function handlePointerDown(event: React.PointerEvent) {
+    if (!windowRef.current) return
+
+    const rect = windowRef.current.getBoundingClientRect()
+
+    // Offset between mouse click and window top-left
+    const offsetX = event.clientX - rect.left
+    const offsetY = event.clientY - rect.top
+
+    setOffset({ x: offsetX, y: offsetY })
+    setDragging(true)
+
+    // Capture pointer so we still get events even if mouse leaves
+    ;(event.target as HTMLElement).setPointerCapture(event.pointerId)
+  }
+
+  function handlePointerMove(event: React.PointerEvent) {
+    if (!dragging || !windowRef.current) return
+
+    const boundary = window.document.body.clientWidth
+    const rect = windowRef.current.getBoundingClientRect()
+    const newX = event.clientX - offset.x
+    const newY = event.clientY - offset.y
+
+    // HANDLE BOUNDARY
+    // if ((rect.left + rect.width + 5) >= boundary) return
+    // else {
+    windowRef.current.style.transform = ''
+    windowRef.current.style.left = `${newX}px`
+    windowRef.current.style.top = `${newY}px`
+    // }
+  }
+
+  function handlePointerUp(event: React.PointerEvent) {
+    setDragging(false)
+    ;(event.target as HTMLElement).releasePointerCapture(event.pointerId)
+  }
+
   return (
     <div
-      className="opacity-0 animate-fadeIn font-ubuntu text-lg flex flex-col disabled relative p-2 overflow-x-hidden overflow-y-auto h-[calc(100%-40px)]"
-      style={{ scrollbarWidth: 'thin' }}
+      ref={windowRef}
+      style={{
+        top: `50%`,
+        left: `50%`,
+        transform: `translateY(-50%) translateX(-50%)`,
+      }}
+      className="animate-screen-on absolute overflow-hidden max-w-[50rem] max-h-[80dvh] sm:max-h-[37.5rem] rounded-lg border-x-2 border-b-2 border-slate-500 shadow-xl bg-black"
     >
-      <textarea
-        ref={textAreaRef}
-        value={welcome}
-        onChange={() => null}
-        disabled
-        spellCheck={false}
-        className="p-0 outline-none overflow-hidden text-slate-400 resize-none"
+      <div
+        onPointerDown={handlePointerDown}
+        onPointerMove={handlePointerMove}
+        onPointerUp={handlePointerUp}
+        className="w-full h-10 bg-slate-500 relative after:content-[''] after:absolute after:h-5 after:w-full after:bg-white/20 after:rounded-b-lg before:content-[''] before:absolute before:h-10 before:w-full before:backdrop-blur-xs before:z-10"
       />
-      {history.length > 0
-        ? history.map((line, index) => {
-            if (line.type === 'user') {
-              return (
-                <div
-                  key={`index=${index}-L=${line.value.length}`}
-                  className="flex"
-                >
-                  {line.type === 'user' ? <TerminalUser /> : null}
-                  <p>{line.value}</p>
-                </div>
-              )
-            } else {
-              return (
-                <pre
-                  key={`index=${index}-L${line.value.length}`}
-                  className="whitespace-pre"
-                >
-                  {line.value}
-                </pre>
-              )
-            }
-          })
-        : null}
-      <div className="w-full flex">
-        <TerminalUser />
-        {/* <span className="animate-carret">|</span> */}
-        <input
-          ref={inputRef}
-          value={command}
-          onChange={({ target }) => setCommand(target.value)}
-          onKeyDown={handleKey}
-          type="text"
-          className="outline-none w-full"
-          autoFocus
+      <div
+        className="opacity-0 animate-fadeIn font-ubuntu text-lg flex flex-col disabled relative p-2 overflow-x-hidden overflow-y-auto h-[calc(100%-40px)]"
+        style={{ scrollbarWidth: 'thin' }}
+      >
+        <textarea
+          ref={textAreaRef}
+          value={welcome}
+          onChange={() => null}
+          disabled
+          spellCheck={false}
+          className="p-0 outline-none overflow-hidden text-slate-400 resize-none"
         />
+        {history.length > 0
+          ? history.map((line, index) => {
+              if (line.type === 'user') {
+                return (
+                  <div
+                    key={`index=${index}-L=${line.value.length}`}
+                    className="flex"
+                  >
+                    {line.type === 'user' ? <TerminalUser /> : null}
+                    <p>{line.value}</p>
+                  </div>
+                )
+              } else {
+                return (
+                  <pre
+                    key={`index=${index}-L${line.value.length}`}
+                    className="whitespace-pre"
+                  >
+                    {line.value}
+                  </pre>
+                )
+              }
+            })
+          : null}
+        <div className="w-full flex">
+          <TerminalUser />
+          {/* <span className="animate-carret">|</span> */}
+          <input
+            ref={inputRef}
+            value={command}
+            onChange={({ target }) => setCommand(target.value)}
+            onKeyDown={handleKey}
+            type="text"
+            className="outline-none w-full"
+            autoFocus
+          />
+        </div>
       </div>
     </div>
   )
